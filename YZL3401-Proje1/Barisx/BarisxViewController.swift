c31292a23cfa158c94d84be1b3f8192a4fa26e26
Barış Köseoğlu <>
2024-01-06 11:45:46 +0300

    baris

---
+++ YZL3401-Proje1/Barisx/BarisxViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BarisxViewController.swift
+//  YZL3401-Proje1
+//
+//  Created by Barış Köseoğlu on 6.01.2024.
+//
+
+import UIKit
+
+class BarisxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
