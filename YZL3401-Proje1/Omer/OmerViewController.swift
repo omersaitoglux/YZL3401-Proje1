a6c917f0db5f2aea3e4f9bc339f9be698597b8ac
Ömer Hattab Saitoğlu <>
2024-01-06 10:18:42 +0300

    Omer adlı grup oluşturuldu

---
+++ YZL3401-Proje1/Omer/OmerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OmerViewController.swift
+//  YZL3401-Proje1
+//
+//  Created by Ömer Hattab Saitoğlu on 6.01.2024.
+//
+
+import UIKit
+
+class OmerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
