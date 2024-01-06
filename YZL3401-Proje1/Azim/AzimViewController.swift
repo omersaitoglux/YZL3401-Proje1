4cc37da9558fb502e38e5c30f69962d0793e9299
azim <azimkose@gmail.com>
2024-01-06 08:54:50 +0000

    no message

---
+++ YZL3401-Proje1/Azim/AzimViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AzimViewController.swift
+//  YZL3401-Proje1
+//
+//  Created by azim on 06/01/2024.
+//
+
+import UIKit
+
+class AzimViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
