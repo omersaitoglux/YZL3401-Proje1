8a0cd4738b74a8534c7e44a5f3228e9504a4a5b5
Melisa Guner <melisa-guner@hotmail.com>
2024-01-06 11:22:33 +0300

    Melisa adlı grup oluşturuldu

---
+++ YZL3401-Proje1/Melisa/MelisaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MelisaViewController.swift
+//  YZL3401-Proje1
+//
+//  Created by Melisa Guner on 6.01.2024.
+//
+
+import UIKit
+
+class MelisaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
